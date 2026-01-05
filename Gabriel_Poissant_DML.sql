ded3161e1feafb0eebb41e2ad73ed550e99f5046
Gabe Poissant <gabrielpoissant@gmail.com>
2026-01-05 14:35:29 -0800

    Add files via upload

---
+++ Gabriel_Poissant_DML.sql
@@ -0,0 +1,203 @@
+-- YEAR
+INSERT INTO YEAR
+(`Year`)
+SELECT DISTINCT `year` FROM BULK_CAR_SALES ORDER BY year;
+
+ALTER TABLE BULK_CAR_SALES
+ADD Year_ID INT;
+
+UPDATE BULK_CAR_SALES b, `YEAR` y
+SET b.Year_ID = y.Year_ID
+WHERE b.year = y.Year;
+
+-- MAKE
+INSERT INTO MAKE
+(Make)
+SELECT DISTINCT make FROM BULK_CAR_SALES;
+
+ALTER TABLE BULK_CAR_SALES
+ADD Make_ID INT;
+
+UPDATE BULK_CAR_SALES b, MAKE m
+SET b.Make_ID = m.Make_ID
+WHERE b.make = m.Make;
+
+-- MODEL
+INSERT INTO MODEL
+(Model)
+SELECT DISTINCT model FROM BULK_CAR_SALES;
+
+ALTER TABLE BULK_CAR_SALES
+ADD Model_ID INT;
+
+UPDATE BULK_CAR_SALES b, MODEL m
+SET b.Model_ID = m.Model_ID
+WHERE b.model = m.Model;
+
+-- TRIM_TYPE
+INSERT INTO TRIM_TYPE
+(Trim_Type)
+SELECT DISTINCT trim FROM BULK_CAR_SALES;
+
+ALTER TABLE BULK_CAR_SALES
+ADD Trim_Type_ID INT;
+
+UPDATE BULK_CAR_SALES b, TRIM_TYPE t
+SET b.Trim_Type_ID = t.Trim_Type_ID
+WHERE b.trim = t.Trim_Type;
+
+-- BODY
+INSERT INTO BODY
+(Body)
+SELECT DISTINCT body FROM BULK_CAR_SALES;
+
+ALTER TABLE BULK_CAR_SALES
+ADD Body_ID INT;
+
+UPDATE BULK_CAR_SALES b, BODY bo
+SET b.Body_ID = bo.Body_ID
+WHERE b.body = bo.Body;
+
+-- TRANSMISSION
+INSERT INTO TRANSMISSION
+(Transmission)
+SELECT DISTINCT transmission FROM BULK_CAR_SALES;
+
+ALTER TABLE BULK_CAR_SALES
+ADD Transmission_ID INT;
+
+UPDATE BULK_CAR_SALES b, TRANSMISSION t
+SET b.Transmission_ID = t.Transmission_ID
+WHERE b.transmission = t.Transmission;
+
+-- STATE
+INSERT INTO STATE
+(State)
+SELECT DISTINCT state FROM BULK_CAR_SALES ORDER BY state;
+
+ALTER TABLE BULK_CAR_SALES
+ADD State_ID INT;
+
+UPDATE BULK_CAR_SALES b, STATE s
+SET b.State_ID = s.State_ID
+WHERE b.state = s.State;
+
+-- SELLER
+INSERT INTO SELLER
+(Seller)
+SELECT DISTINCT seller FROM BULK_CAR_SALES;
+
+ALTER TABLE BULK_CAR_SALES
+ADD Seller_ID INT;
+
+UPDATE BULK_CAR_SALES b, SELLER s
+SET b.Seller_ID = s.Seller_ID
+WHERE b.seller = s.Seller;
+
+
+
+-- Color
+INSERT INTO COLOR
+(Color)
+SELECT DISTINCT color FROM BULK_CAR_SALES
+UNION 
+SELECT DISTINCT interior FROM BULK_CAR_SALES;
+
+ALTER TABLE BULK_CAR_SALES
+ADD Color_ID INT;
+
+ALTER TABLE BULK_CAR_SALES
+ADD Interior_Color_ID INT; 
+
+UPDATE BULK_CAR_SALES b, COLOR c
+SET b.Color_ID = c.Color_ID
+WHERE b.color = c.Color;
+
+UPDATE BULK_CAR_SALES b, COLOR c
+SET b.Interior_Color_ID = c.Color_ID
+WHERE b.interior = c.Color;
+
+
+
+-- CAR_SALE
+INSERT INTO CAR_SALE
+(VIN,Year_ID,Make_ID,Model_ID,Trim_Type_ID,Body_ID,Transmission_ID,Color_ID,
+Interior_Color_ID,Car_Condition,Odometer,MMR,Selling_Price,State_ID,Seller_ID,Sale_Date)
+SELECT vin,Year_ID,Make_ID,Model_ID,Trim_Type_ID,Body_ID,Transmission_ID,Color_ID,
+Interior_Color_ID,`condition`,Odometer,mmr,sellingprice,State_ID,Seller_ID,saledate
+FROM BULK_CAR_SALES;
+
+-- vw_ALL
+CREATE VIEW vw_ALL
+as 
+SELECT 
+y.Year as `year`,
+m.Make as make,
+mo.Model as model,
+t.Trim_Type as trim,
+b.Body as body,
+tr.Transmission as transmission,
+cs.VIN as vin,
+s.State as state,
+cs.Car_Condition as `condition`,
+cs.Odometer as odometer,
+c.Color as color,
+ic.Color as interior,
+se.Seller as seller,
+cs.MMR as mmr,
+cs.Selling_Price as sellingprice,
+cs.Sale_Date as saledate
+FROM CAR_SALE cs
+INNER JOIN `YEAR` y ON cs.Year_ID = y.Year_ID 
+INNER JOIN MAKE m on cs.Make_ID = m.Make_ID
+INNER JOIN MODEL mo ON cs.Model_ID = mo.Model_ID
+INNER JOIN TRIM_TYPE t ON cs.Trim_Type_ID = t.Trim_Type_ID
+INNER JOIN BODY b on cs.Body_ID = b.Body_ID
+INNER JOIN TRANSMISSION tr ON cs.Transmission_ID = tr.Transmission_ID
+INNER JOIN STATE s ON cs.State_ID = s.State_ID
+INNER JOIN COLOR c ON cs.Color_ID = c.Color_ID
+INNER JOIN COLOR ic ON cs.Interior_Color_ID = ic.Color_ID
+INNER JOIN SELLER se ON cs.Seller_ID = se.Seller_ID;
+
+
+-- vw_CARS
+CREATE VIEW vw_CARS
+as 
+SELECT 
+y.Year as `year`,
+m.Make as make,
+mo.Model as model,
+t.Trim_Type as trim,
+b.Body as body,
+tr.Transmission as transmission,
+cs.VIN as vin,
+cs.Car_Condition as `condition`,
+cs.Odometer as odometer,
+c.Color as color,
+ic.Color as interior,
+cs.MMR as mmr,
+cs.Selling_Price as sellingprice
+FROM CAR_SALE cs
+INNER JOIN `YEAR` y ON cs.Year_ID = y.Year_ID 
+INNER JOIN MAKE m on cs.Make_ID = m.Make_ID
+INNER JOIN MODEL mo ON cs.Model_ID = mo.Model_ID
+INNER JOIN TRIM_TYPE t ON cs.Trim_Type_ID = t.Trim_Type_ID
+INNER JOIN BODY b on cs.Body_ID = b.Body_ID
+INNER JOIN TRANSMISSION tr ON cs.Transmission_ID = tr.Transmission_ID
+INNER JOIN COLOR c ON cs.Color_ID = c.Color_ID
+INNER JOIN COLOR ic ON cs.Interior_Color_ID = ic.Color_ID;
+
+-- vw_SELLER
+CREATE VIEW vw_SELLER
+as 
+SELECT 
+s.State as state,
+se.Seller as seller,
+cs.Sale_Date as saledate
+FROM CAR_SALE cs
+INNER JOIN STATE s ON cs.State_ID = s.State_ID
+INNER JOIN SELLER se ON cs.Seller_ID = se.Seller_ID;
+
+
+
+
---
+++ stuff.sql
@@ -0,0 +1,37 @@
+SET GLOBAL local_infile = 1;
+
+DROP TABLE BULK_CAR_SALES;
+
+CREATE TABLE BULK_CAR_SALES (
+    year INT NOT NULL,
+    make VARCHAR(50),
+    model VARCHAR(100),
+    trim VARCHAR(100),
+    body VARCHAR(50),
+    transmission VARCHAR(20),
+    vin CHAR(17),
+    state CHAR(2),
+    `condition` SMALLINT,
+    odometer INT,
+    color VARCHAR(30),
+    interior VARCHAR(30),
+    seller VARCHAR(255),
+    mmr INT,
+    sellingprice INT,
+    saledate VARCHAR(100)
+) ENGINE=InnoDB;
+
+
+SET autocommit=0;
+SET unique_checks=0;
+SET foreign_key_checks=0;
+
+SET GLOBAL local_infile = 1;
+
+
+LOAD DATA LOCAL INFILE 'C:\Users\gabri\Documents\PittSCI\Courses\CMPINF_2110\Conceptual Design Project\car_prices_cleaned.csv'
+INTO TABLE BULK_CAR_SALES
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS; -- Use this if your file has a header row
